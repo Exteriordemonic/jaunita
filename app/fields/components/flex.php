8f6e7b5e31675bc0061064c80a47b4ee85545c0d
Expenso <kmirosz6@gmail.com>
2020-08-17 15:46:39 +0200

    flex section

--- app/fields/components/flex.php
+++ app/fields/components/flex.php
@@ -6,6 +6,25 @@ use StoutLogic\AcfBuilder\FieldsBuilder;
 $flex = new FieldsBuilder('flex');
   
 $flex
-    ->addText('flex');
+    ->addGroup('options', ['label'=>'', 'wrapper'=>['width'=>'50%']])
+        ->addImage('image', ['label'=>'Image 600x600'])
+        ->addTrueFalse('pattern', ['label'=>'Pattern'])
+        ->addSelect('bg_color',  ['label'=>'Background color', 'wrapper'=>['width'=>'50%']])
+            ->addChoice('light')
+            ->addChoice('gray')
+            ->setDefaultValue('light')
+        ->addSelect('direction',  ['label'=>'Image direction', 'wrapper'=>['width'=>'50%']])
+            ->addChoice('left')
+            ->addChoice('right')
+            ->setDefaultValue('left')
+        ->addNumber('offset_top', ['label'=>'Image offset top in %','default_value'=>0, 'wrapper'=>['width'=>'50%']])
+        ->addNumber('offset_bottom', ['label'=>'Image offset bottom in %','default_value'=>0, 'wrapper'=>['width'=>'50%']])
+    ->endGroup()
+    ->addGroup('content', ['label'=>'', 'wrapper'=>['width'=>'50%']])
+        ->addText('title', ['label'=>'Title'])
+        ->addText('subtitle', ['label'=>'SubTitle'])
+        ->addTextarea('text', ['label'=>'Text', 'new_lines'=>'br'])
+        ->addLink('link', ['label'=>'Link'])
+    ;
     
 return $flex;
---
+++ resources/views/sections/flex.blade.php
@@ -0,0 +1,48 @@
+@php
+    $options = $data['options'];
+    $img = $options['image']['ID'];
+    $color = $options['bg_color'];
+    $direction = $options['direction'];
+    $offset_top = $options['offset_top'] ? $options['offset_top'] : 0;
+    $offset_bottom = $options['offset_bottom'] ? $options['offset_bottom'] : 0;
+    $pattern = $options['pattern'] ? 'pattern' : '';
+
+    $content = $data['content'];
+    $title = $content['title'];
+    $subtitle = $content['subtitle'];
+    $text = $content['text'];
+    $link = $content['link'];
+@endphp
+
+<section class="flex flex--{{ $color }} flex--{{ $direction }} flex--{{ $pattern }} section">
+    <div class="flex__wrapper container">
+        <figure class="flex__image-wrapper" style="margin-top: {{ -$offset_top }}%; margin-bottom: {{ -$offset_bottom }}%;">
+            {!! image($img, 'full', 'flex__image') !!}
+        </figure>
+        <article class="flex__content">
+            @if ($title)
+            <h2 class="flex__title">
+                {{ $title }}
+            </h2>
+            @endif
+
+            @if ($subtitle)
+            <h3 class="flex__subtitle">
+                {{ $subtitle }}
+            </h3>
+            @endif
+
+            @if ($text)
+            <p class="flex__text">
+                {!! $text !!}
+            </p>
+            @endif
+
+            @if ($link)
+            <a href="{{ $link['link'] }}" class="button button--light" target="{{ $link['target'] }}">
+                {{ $link['title'] }}
+            </a>
+            @endif
+        </article>
+    </div>
+</section>
